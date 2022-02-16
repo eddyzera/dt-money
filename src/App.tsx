449c98fe2e3443756fa20da6882bbbb558939ab4
eddyzera <edgar.nascimento@newgo.com.br>
2022-02-16 20:12:04 -0300

    [style]: create global style for all aplication

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import {GlobalStyle} from './styles/globalStyle'
 
 export const App: React.FC = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Hello World</h1>
+    </>
+  )
 }
---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,53 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+
+  :root {
+    --brackground: #F0F2F5;
+    --red: #e52e4d;
+    --blue: #5429cc;
+    --blue-light: #6933ff;
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+    --shape: #FFF
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size:  93.75%; // 15px
+    }
+
+    @media (max-width: 720px) {
+      font-size:  87.5%; // 14px
+    }
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: var(--brackground);
+    --webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`
