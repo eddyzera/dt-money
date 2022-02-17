66d7371d70c0f9731e7bb1d4bbf59b49af87ccef
eddyzera <edgar.nascimento@newgo.com.br>
2022-02-17 13:17:55 -0300

    [style/feat]: create component TransactionTable and your style

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary"
+import { TransactionsTable } from "../TransactionsTable"
 import { Container } from "./styles"
 
 export const Dasboard = () => {
   return (
     <Container>
       <Summary />
+      <TransactionsTable />
     </Container>
   )
 }
