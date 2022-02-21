2057e18d9dd113ccbac5f5f9aa76f0d3bffe52aa
eddyzera <edgar.nascimento@newgo.com.br>
2022-02-21 12:10:15 -0300

    [refactor]: take the data of api, make map and show on UI

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,12 +1,23 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { api } from "../../services/api"
 import { Container } from "./styles"
 
+interface Transaction {
+  id: number,
+  title: string,
+  amount: number,
+  type: string,
+  category: string,
+  createdAt: string
+}
+
 export const TransactionsTable = () => {
+
+  const [transactions, setTransactions] = useState<Array<Transaction>>([])
 
   useEffect(() => {
     api.get('transactions')
-      .then(({data}) => console.log(data))
+      .then(({data}) => setTransactions(data.transactions))
   }, [])
 
   return (
@@ -21,19 +32,16 @@ export const TransactionsTable = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Desenvolvimento web site</td>
-            <td className="deposit">R$: 12.000,00</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
-
-          <tr>
-            <td>Aluguel</td>
-            <td className="withdraw">- R$: 1000,00</td>
-            <td>Casa</td>
-            <td>17/02/2021</td>
-          </tr>
+          { transactions.map(transaction => {
+            return (
+              <tr key={transaction.id}>
+                <td>{transaction.title}</td>
+                <td className={transaction.type}>{transaction.amount}</td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+            )
+          }) }
         </tbody>
       </table>
     </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,29 @@ createServer({
     transaction: Model
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer de Website',
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00;00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-12 11:00:00')
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
     this.get('/transactions', () => {
